29596b1fc6e22fe97ef86112ab8130ec0bf612fa
Allifiando Dewantoro <Allifiando Dewantoro>
2017-09-13 06:59:11 +0700

    first

---
+++ app/src/main/java/com/example/ando/peta/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.ando.peta;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn1 = (Button) findViewById(R.id.btn1);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
